494c9e88f50c2d0e2a2fb20d4f44b47914c02ba9
DNG <dungbv1809@gmail.com>
2024-05-08 02:45:06 +0700

    code day 2

---
+++ StoveController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveController : MonoBehaviour
+{
+    public GameObject menuStove;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
