b861dd25485e503b3d2cc1b24966efc9d420f905
DNG <dungbv1809@gmail.com>
2024-05-07 09:13:52 +0700

    Add code

--- CameraController.cs
+++ CameraController.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = player.transform.position + new Vector3(0,0, -10);
